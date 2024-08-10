883b34ad948c81c3d0b19dc08c8644de42e47e25
Shivam Rai <shivam.r@inspironlabs.com>
2024-08-10 23:22:24 +0530

    table done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Edit from "./components/edit/Edit";
+import FetchData from "./components/fetch/FetchData";
+
+const App = () =>{
+  return(
+    <div>
+      <Edit/>
+      <FetchData/>
+    </div>
+  )
+}
+export default App;
---
+++ src/components/fetch/FetchData.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import './fetchData.css'
+
+const FetchData = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const tableFetchData = async() =>{
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await res.json()
+        setData(data)
+    }
+    useEffect(() =>{
+        tableFetchData()
+    },[])
+  return (
+    <div>
+      
+            <table>
+                <tr>
+                <th>Product ID</th>
+                <th>Product Name</th>
+                </tr>
+                {data.map((ele) =>{
+                    return(
+                <tr>
+                    <td>{ele.id}</td>
+                    <td>{ele.title}</td>
+                </tr>
+                    )
+                })}
+            </table>
+      
+    </div>
+  )
+}
+
+export default FetchData;
